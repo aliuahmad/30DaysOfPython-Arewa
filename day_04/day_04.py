d7c38b89d3a6f6416f2dc4da98ef8506af9a3d96
Ahmad Aliyu <aliunabab@gmail.com>
2026-05-02 18:41:53 +0100

    Day 4 completed - strings

---
+++ day_04/day_04.py
@@ -0,0 +1,25 @@
+print("DAY 4 STARTED")
+
+# Strings
+first_name = "Ahmad"
+last_name = "Aliyu"
+
+full_name = first_name + " " + last_name
+print("Full name:", full_name)
+
+# String length
+print("Length:", len(full_name))
+
+# String indexing
+print("First letter:", full_name[0])
+print("Last letter:", full_name[-1])
+
+# String methods
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+
+# Substring
+print(full_name[0:5])
+
+print("DAY 4 COMPLETED 🎉")
